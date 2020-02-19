5b711b31f87f04c0653ca819190082e6aa4029bb
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-19 20:35:54 +0200

    Removed default views and controller

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
-
 Route::middleware(['auth'])->group(function () {
     Route::resource('tickets', 'TicketsController');
     Route::resource('tickets.replies', 'RepliesController')->only(['store', 'destroy']);
