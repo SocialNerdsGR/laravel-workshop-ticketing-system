219448afc3139dc23dbe629bf50d722e6c3851ca
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-19 20:43:41 +0200

    Removed default views and controller

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,15 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    protected $fillable = ['title', 'content', 'user_id'];
-
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function replies()
-    {
-        return $this->hasMany(Reply::class);
     }
 }
