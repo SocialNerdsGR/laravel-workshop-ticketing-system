5ff64cf7ba363975433932511cfa46310ec33e23
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-19 20:27:57 +0200

    Removed next steps

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,3 @@
 <?php
 
 Auth::routes();
-
-Route::get('/', 'HomeController@index')->name('home');
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('tickets', 'TicketsController');
-    Route::resource('tickets.replies', 'RepliesController')->only(['store', 'destroy']);
-});
