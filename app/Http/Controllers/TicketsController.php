5396bc4589efec985ac058177c9f091b75868e6d
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-19 21:01:13 +0200

    Removed next steps

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -11,7 +11,6 @@ class TicketsController extends Controller
 
     public function __construct()
     {
-        $this->authorizeResource(Ticket::class, 'ticket');
     }
 
     /**
@@ -65,8 +64,6 @@ class TicketsController extends Controller
      */
     public function show(Ticket $ticket)
     {
-        $replies = $ticket->replies()->with('user')->latest()->paginate(5);
-        return view('tickets.show', compact('ticket', 'replies'));
     }
 
     /**
@@ -77,7 +74,6 @@ class TicketsController extends Controller
      */
     public function edit(Ticket $ticket)
     {
-        return view('tickets.edit', ['ticket' => $ticket]);
     }
 
     /**
@@ -89,15 +85,6 @@ class TicketsController extends Controller
      */
     public function update(Request $request, Ticket $ticket)
     {
-        $validatedData = request()->validate([
-            'title' => 'required|min:5|unique:tickets,title,' . $ticket->id,
-            'content' => 'required|max:255'
-        ]);
-
-        $ticket->fill($validatedData);
-        $ticket->save();
-
-        return redirect(action('TicketsController@edit', ['ticket' => $ticket->id]));
     }
 
     /**
@@ -108,7 +95,5 @@ class TicketsController extends Controller
      */
     public function destroy(Ticket $ticket)
     {
-        $ticket->delete();
-        return redirect(action('TicketsController@index'));
     }
 }
