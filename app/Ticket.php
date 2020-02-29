612a934d0dac7e6e402c4ce3f7ca055471c42b16
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-29 21:26:50 +0200

    Added fillable array on Ticket model

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    protected $fillable = ['title', 'content', 'user_id'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
