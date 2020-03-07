d28c38fa7752d602de477993d058ebf6e1f7bc4c
Thanos Korakas <thanoskorakas@gmail.com>
2020-03-07 22:25:23 +0200

    Removed link to create page

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <h2>Tickets</h2>
-<a href={{action('TicketsController@create')}}>Create new ticket</a>
 <ul>
   @foreach($tickets as $ticket)
   <li><a href={{action('TicketsController@show', ['ticket' => $ticket->id])}}>{{$ticket->title}}</a></li>
