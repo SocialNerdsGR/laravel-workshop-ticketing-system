490235d474366254328070f2026159d3ca3a07f7
Thanos Korakas <thanoskorakas@gmail.com>
2020-03-08 08:06:26 +0200

    Imported classes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('tickets', 'TicketsController');
     Route::resource('tickets.replies', 'RepliesController')->only(['store', 'destroy']);
+});
+
+Route::get('/test/{reply}', function (App\Reply $reply) {
+    dd($reply);
 });
